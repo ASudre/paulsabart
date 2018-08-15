f3fa70713f62dbd7c7407c8ee5b41acd6a686dc6
Arthur Sudre <arthursudre@gmail.com>
2018-08-15 16:32:36 +0200

    refactor(back): remove unused files utils function

--- backend/src/files.utils.php
+++ backend/src/files.utils.php
@@ -11,34 +11,6 @@ function getDirContents($path) {
   return $files;
 }
 
-function buildFilesObjectFromFilesArray($filesArray) {
-  $filesObject = [];
-  foreach ($filesArray as $value) {
-      $parsed = explode('/', $value);
-      if (count($parsed) < 3) {
-          break;
-      }
-      $filesPaths = implode('/', array_slice($parsed, 2));
-      if (array_key_exists($parsed[0], $filesObject)) {
-          $currentFolderFiles = [];
-          if (array_key_exists($parsed[1], $filesObject[$parsed[0]])) {
-              $currentFolderFiles = $filesObject[$parsed[0]][$parsed[1]];
-          }
-          $filesObject[$parsed[0]] =
-              array_merge(
-                  $filesObject[$parsed[0]],
-                  array($parsed[1] => array_merge($currentFolderFiles, [$filesPaths]))
-              );
-      } else {
-          $filesObject[$parsed[0]] = array($parsed[1] => [$filesPaths]);                
-      }
-  }
-  if(count($filesObject) == 0) {
-      return new stdClass();
-  }
-  return $filesObject;
-}
-
 function fileParsing($folderPath, $filePath) {
   $parsed;
   $exploded = explode('/', str_replace($folderPath . '/', '', $filePath));
