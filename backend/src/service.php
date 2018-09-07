df8e30e36cd7b20acde210181b14f640ed92abab
Arthur Sudre <arthursudre@gmail.com>
2018-09-07 11:49:59 +0200

    feat(back): generate comment from file name

--- backend/src/service.php
+++ backend/src/service.php
@@ -27,7 +27,7 @@ class Service {
 
   public function insert($files) {
     /* Prepared statement, stage 1: prepare */
-    if (!($stmt = $this->conn->prepare("INSERT INTO files(theme, category, file_path, file_name, updated_at) VALUES (?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE theme=?, category=?, file_path=?, updated_at=?;"))) {
+    if (!($stmt = $this->conn->prepare("INSERT INTO files(theme, category, file_path, file_name, comment, updated_at) VALUES (?, ?, ?, ?, ?, ?) ON DUPLICATE KEY UPDATE theme=?, category=?, file_path=?, updated_at=?;"))) {
       echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
     }
     
@@ -36,9 +36,10 @@ class Service {
     $category = $files[0][1];
     $file_path = $files[0][2];
     $file_name = $files[0][3];
+    $comment = explode('.', $files[0][3])[0];
     $date = new DateTime();
     $updated_at = $date->format('Y-m-d H:i:s');
-    if (!$stmt->bind_param("sssssssss", $theme, $category, $file_path, $file_name, $updated_at, $theme, $category, $file_path, $updated_at)) {
+    if (!$stmt->bind_param("ssssssssss", $theme, $category, $file_path, $file_name, $comment, $updated_at, $theme, $category, $file_path, $updated_at)) {
         echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
     }
 
@@ -52,6 +53,7 @@ class Service {
       $category = $files[$i][1];
       $file_path = $files[$i][2];
       $file_name = $files[$i][3];
+      $comment = explode('.', $files[$i][3])[0];
       if (!$stmt->execute()) {
           echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
       }
