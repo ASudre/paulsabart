f7e5696aee452ad852045b6239e6ee494f96873d
Arthur Sudre <arthursudre@gmail.com>
2019-05-08 15:42:49 +0200

    migration(): remove old front

--- front_old/src/config/config.js
+++
@@ -1,4 +0,0 @@
-import prodConfig from './config.production';
-import devConfig from './config.development';
-
-export default process.env.NODE_ENV === 'production' ? prodConfig : devConfig;
--- front_old/src/index.js
+++
@@ -1,7 +0,0 @@
-import 'babel-polyfill';
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './components/App.component';
-
-ReactDOM.render(<App />, document.getElementById('index'));
