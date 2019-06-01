cfa8a2c0c2ad2876828751774473db4578108628
Arthur Sudre <arthursudre@gmail.com>
2019-06-01 21:06:25 +0200

    fix(backend): fix env to production

--- backend/index.php
+++ backend/index.php
@@ -5,7 +5,7 @@ date_default_timezone_set('Europe/Paris');
 require __DIR__ . '/vendor/jacwright/restserver/source/Jacwright/RestServer/RestServer.php';
 require __DIR__ . '/src/routes.php';
 
-$server = new \Jacwright\RestServer\RestServer('debug');
+$server = new \Jacwright\RestServer\RestServer('production');
 $server->useCors = true;
 $server->allowedOrigin = array('http://localhost:3000');
 $server->addClass('routes');
